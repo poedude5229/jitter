6a5840463d3d5dc76755ad5977cac3d101e1049f
Joseph <poedude5229@gmail.com>
2024-12-07 14:08:38 -0500

    Adding seed commands, in progress 3

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,5 +3,16 @@ from sqlalchemy.sql import text
 
 def seed_users():
     bigjoe = User(
-        username="bigjoe", email='edgelord420@zune.net', password='password69',
+        username="bigjoe", email='edgelord420@zune.net', password='password69', bio='Founder. Innovator. I made this website for real for real.', profile_pic='', profile_banner=''
     )
+
+    db.session.add(bigjoe)
+    db.session.commit()
+
+def undo_users():
+    if environment == 'production':
+        db.session.execute(f"TRUCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM users"))
+
+    db.session.commit()
