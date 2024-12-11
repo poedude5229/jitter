52d8d20de62432db19652bf0f423b1af8e7e2d12
Joseph <poedude5229@gmail.com>
2024-12-11 16:04:12 -0500

    Tables done without associations

---
+++ app/models/ember_like.py
@@ -0,0 +1,18 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class EmberLike(db.Model):
+    __tablename__ = 'emberlikes'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    ember_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'ember_id': self.ember_id,
+            'user_id': self.user_id
+        }
