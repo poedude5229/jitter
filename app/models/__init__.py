27a47a5c2f84a0d6c2f355ffedc4026e8da4df3c
Joseph <poedude5229@gmail.com>
2024-12-14 22:44:04 -0500

    starting seeds

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,8 @@
 from .db import db, environment, SCHEMA
 from .user import User
+from .flicker import Flicker
+from .ember import Ember
+from .follow import Follow
+from .spark import Spark
+from .ember_like import EmberLike
+
