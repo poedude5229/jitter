5a4d849ea7025a70ad18116665c3bfe988365f3a
Joseph <poedude5229@gmail.com>
2025-01-18 22:10:52 -0500

    Embers seed commands successful. Sample data added

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .flickers import seed_flickers, undo_flickers
+from .embers import seed_embers, undo_embers
 from app.models.db import db, environment, SCHEMA
 
 seed_commands = AppGroup('seed')
@@ -8,12 +9,15 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
+        undo_embers()
         undo_flickers()
         undo_users()
     seed_users()
     seed_flickers()
+    seed_embers()
 
 @seed_commands.command('undo')
 def undo():
+    undo_embers()
     undo_flickers()
     undo_users()
--- app/seeds/embers.py
+++ app/seeds/embers.py
@@ -5,7 +5,11 @@ def seed_embers():
     e1 = Ember(
         user_id=4, post_id=1, comment_text="This is the first ever comment on FireFly! Woohoo!"
     )
+    e2 = Ember(user_id=1, post_id=2, comment_text="Omg hey John Pork! You are the greatest travel blogger in existence. Will you please visit my hometown in WV?")
+    e3 = Ember(user_id=1, post_id=2, comment_text="We love you Ben! Hope to hear you and Puffin back on the radio this year! LPOTL is not funny without you.")
     db.session.add(e1)
+    db.session.add(e2)
+    db.session.add(e3)
     db.session.commit()
 
 def undo_embers():
